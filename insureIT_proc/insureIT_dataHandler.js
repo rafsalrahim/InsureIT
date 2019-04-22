560d64ebd9adb54731e0f4f1ae8ed1dd4ecf30dd
rafsal <rafsalkrahim@gmail.com>
2019-04-23 01:41:02 +0530

    >second handler

--- insureIT_proc/insureIT_dataHandler.js
+++ insureIT_proc/insureIT_dataHandler.js
@@ -15,3 +15,22 @@ const MIN_VALUE = 0;
 const CJ_FAMILY = 'datainsure';
 const CJ_NAMESPACE = _hash(CJ_FAMILY).substring(0, 6);
 let addrList = [];
+
+class InsureITData extends TransactionHandler{
+    constructor(){
+        super(CJ_FAMILY,['1.0'],[CJ_NAMESPACE]);
+
+    }
+
+    decodepayload(payload){
+        const payloadDecoded={
+            from : payload[0],
+            to: payload[1],
+            amt: payload[2],
+            proc: payload[4],
+            action: payload[5],
+
+
+        }
+    }
+}
